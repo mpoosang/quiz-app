a236465d50bf68bcaee68c83a0f233fc22c0c729
mpoosang <mpoosang@gmail.com>
2020-11-06 14:46:52 -0800

    added store.js

---
+++ store.js
@@ -0,0 +1,109 @@
+const store = {
+    questions: [
+      {
+        question: 'Name the grab where you grab your heelside edge (between your feet) with your front hand',
+        answers: [
+          'Mindy',
+          'Crail',
+          'Melon',
+          'Indy'
+        ],
+        correctAnswer: 'Melon'
+      },
+      {
+        question: 'Name the jib trick where you pop the tail of your board up and over the feature, landing perpendicular in a sliding position',
+        answers: [
+          'Michael Jackson',
+          'Boardslide',
+          'Noseslide',
+          'Lipslide'
+        ],
+        correctAnswer: 'Lipslide'
+      },
+      {
+        question: 'Name the grab where you grab your heelside edge (between your feet) with your rear hand',
+        answers: [
+          'Stalefish',
+          'Mute',
+          'Japan',
+          'Chicken Salad'
+        ],
+        correctAnswer: 'Stalefish'
+      },
+      {
+        question: 'Name the jib trick where you jump all the way across the feature, landing perpendicular on just the tail of your board',
+        answers: [
+          'Boardslide',
+          'Hardway',
+          'Bluntslide',
+          'Lipslide'
+        ],
+        correctAnswer: 'Bluntslide'
+      },
+      {
+        question: 'Name the grab where you reach between your feet with your back hand, through your legs and grab the heel edge of your snowboard',
+        answers: [
+          'Canadian Bacon',
+          'Shifty',
+          'Method',
+          'Roast Beef'
+        ],
+        correctAnswer: 'Roast Beef'
+      },
+      {
+        question: 'Name the jib trick where you pop off a board or lipslide, spinning 270 in the opposite direction you jumped on',
+        answers: [
+          'Knuckle',
+          'Pretzel',
+          'Hardway',
+          'Rewind'
+        ],
+        correctAnswer: 'Pretzel'
+      },
+      {
+        question: 'Name the grab where your rear hand grabs your toe edge slightly inside your rear foot',
+        answers: [
+          'Indy',
+          'Nosegrab',
+          'Mute',
+          'Tindy'
+        ],
+        correctAnswer: 'Indy'
+      },
+      {
+        question: 'Name the jib trick where you slide straight down the feature, nose first, with weight evenly on both feet',
+        answers: [
+          'Slide',
+          'Boardslide',
+          '50-50',
+          'Press'
+        ],
+        correctAnswer: '50-50'
+      },
+      {
+        question: 'Name the grab where you grab the toe edge (preferably in-between the binding) with your front hand and tweak it out behind your back',
+        answers: [
+          'Japan',
+          'Alley-oop',
+          'Seatbelt',
+          'Rodeo'
+        ],
+        correctAnswer: 'Japan'
+      },
+      {
+        question: 'Name the grab where both of your hands grab the nose, while the rear leg is boned and the front leg is pulled up',
+        answers: [
+          'Taipan',
+          'Cross Rocket',
+          'Cork',
+          'Rocket Air'
+        ],
+        correctAnswer: 'Rocket Air'
+      },
+    ],
+    quizStarted: false,
+    questionNumber: 0,
+    score: 0
+  };
+  
+
